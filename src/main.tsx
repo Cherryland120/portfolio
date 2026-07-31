338b2046c08b5dece3439484f871ce25584bc807
Anointing Tamunowunari-Tasker <anointingtasker2002@gmail.com>
2026-07-31 01:11:46 +0100

    cms: add animator trigger system blog post + fix Vite CDN error

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,13 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Inject devicons CDN stylesheet (avoids Vite import analysis issues with external CDN in index.html)
+const deviconsLink = document.createElement('link');
+deviconsLink.rel = 'stylesheet';
+deviconsLink.href = 'https://cdn.jsdelivr.net/gh/devicons/devicon@latest/devicon.min.css';
+document.head.appendChild(deviconsLink);
+
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
